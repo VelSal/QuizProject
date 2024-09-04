aee0789c48a83dc483088c87b548a05011b056b2
VelSal <v.salvatore@live.be>
2024-09-04 14:00:20 +0200

    Added actions and questions view

--- QuizProject/Controllers/QuizController.cs
+++ QuizProject/Controllers/QuizController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using QuizProject.Data;
 using QuizProject.Models;
+using QuizProject.Services;
 using QuizProject.ViewModels;
 
 namespace QuizProject.Controllers
@@ -9,9 +10,11 @@ namespace QuizProject.Controllers
     public class QuizController : Controller
     {
         private readonly QuizContext _context;
-        public QuizController(QuizContext context)
+        private readonly QuizService _quizService;
+        public QuizController(QuizContext context, QuizService quizService)
         {
             _context = context;
+            _quizService = quizService;
         }
 
         public IActionResult Index()
@@ -48,6 +51,58 @@ namespace QuizProject.Controllers
                 }
             }
             return View(viewModel);
+        }
+
+        // Begin new quiz
+        public async Task<IActionResult> NewQuiz()
+        {
+            await _quizService.CopyAndRandomizeQuestionsAsync();    //Get randomized questions
+            return RedirectToAction(nameof(Question));  //Ask questions
+        }
+
+        // Show question to user
+        public IActionResult Question()
+        {
+            if (!_quizService.HasMoreQuestions())
+            {
+                return RedirectToAction(nameof(PerfectGame));  // Perfect game page when 100% questions answered
+            }
+            var question = _quizService.GetNextQuestion();
+            return View(question);
+        }
+
+        // How did the user answered to the question?
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SubmitedAnswer(int questionId, string selectedAnswer)
+        {
+            var currentQuestion = _quizService.GetNextQuestion();
+
+            //Game over if answer is wrong
+            if (!_quizService.IsCorrectAnswer(currentQuestion, selectedAnswer))
+            {
+                return RedirectToAction(nameof(GameOver)); // Game Over page
+            }
+
+            //If there is no more questions, perfect game
+            if (!_quizService.HasMoreQuestions())
+            {
+                return RedirectToAction(nameof(PerfectGame)); // Perfect game page
+            }
+
+            return RedirectToAction(nameof(Question));
+        }
+
+        //Perfect game view when every questions are answered
+        public IActionResult PerfectGame()
+        {
+            return View();
+        }
+
+        //Game Over when the user chose the wrong answer
+        public IActionResult GameOver()
+        {
+            return View();
         }
     }
 }
---
+++ QuizProject/Services/QuizService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizProject.Data;
+using QuizProject.Models;
+using QuizProject.ViewModels;
+
+namespace QuizProject.Services
+{
+    public class QuizService
+    {
+        private readonly QuizContext _context;
+        private List<Question> _questions;
+        private int _currentQuestionIndex;
+        public QuizService(QuizContext context) 
+        { 
+            _context = context;
+            _questions = new List<Question>();
+            _currentQuestionIndex = 0;
+        }
+
+        public async Task CopyAndRandomizeQuestionsAsync()
+        {
+            _questions = await _context.Questions
+                .OrderBy(q => Guid.NewGuid())   //Generate a random Guid and order them by this Guid = random
+                .ToListAsync();
+        }
+
+        // Get next question if there is one otherwise, return null
+        public Question GetNextQuestion()
+        {
+            if (_currentQuestionIndex >= _questions.Count)
+            {
+                return _questions[_currentQuestionIndex++];
+            }
+            return null;
+        }
+
+        // Is the answer to the question correct?
+        public bool IsCorrectAnswer(Question question, string selectedAnswer)
+        {
+            return question.CorrectAnswer == selectedAnswer;
+        }
+
+        // Is there other questions to ask?
+        public bool HasMoreQuestions()
+        {
+            return _currentQuestionIndex < _questions.Count;
+        }
+       
+    }
+}
